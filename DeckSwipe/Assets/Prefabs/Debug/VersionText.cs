5c5cc353c76059033fe56a0c25985dca39431687
outfrost <kotlet.bahn@gmail.com>
2018-08-06 04:28:01 +0200

    Add game version debug information
    Also make UI scale with screen size

---
+++ DeckSwipe/Assets/Prefabs/Debug/VersionText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VersionText : MonoBehaviour {
+	
+	void Start () {
+		GetComponent<TextMeshProUGUI>().SetText(Application.productName
+		                                        + " "
+		                                        + Application.version
+		                                        + (Application.buildGUID != ""
+		                                           ? ", build " + Application.buildGUID
+		                                           : ""));
+	}
+	
+	void Update () {
+		
+	}
+	
+}
