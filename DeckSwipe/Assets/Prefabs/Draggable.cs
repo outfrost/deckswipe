ce8c0b455928e5cfa96563c5be63956a1729a4a7
outfrost <kotlet.bahn@gmail.com>
2018-08-11 05:40:19 +0200

    Move GameObject around when dragging

--- DeckSwipe/Assets/Prefabs/Draggable.cs
+++ DeckSwipe/Assets/Prefabs/Draggable.cs
@@ -2,9 +2,31 @@
 
 public class Draggable : MonoBehaviour {
 	
-	private void OnMouseDrag()
-	{
-		Debug.Log(Input.mousePosition);
+	private Vector3 snapPosition;
+	private Vector3 dragStartPosition;
+	private Vector3 dragStartPointerPosition;
+	
+	public void SetSnapPosition(Vector3 pos) {
+		snapPosition = pos;
+	}
+	
+	private void Start() {
+		SetSnapPosition(transform.position);
+	}
+	
+	private void OnMouseDown() {
+		dragStartPosition = transform.position;
+		dragStartPointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+	
+	private void OnMouseDrag() {
+		Vector3 positionDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - dragStartPointerPosition;
+		positionDelta.z = dragStartPosition.z;
+		transform.position = dragStartPosition + positionDelta;
+	}
+	
+	private void OnMouseUp() {
+		transform.position = snapPosition;
 	}
 	
 }
