5ccd814d6c8513f457d79635ea1647fbdb86919e
outfrost <kotlet.bahn@gmail.com>
2018-08-15 23:11:54 +0200

    Make stats initialise and display properly

--- DeckSwipe/Assets/Prefabs/Stats.cs
+++ DeckSwipe/Assets/Prefabs/Stats.cs
@@ -1,4 +1,17 @@
-﻿public class Stats {
+﻿using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+	public RectTransform HeatBar;
+	public RectTransform FoodBar;
+	public RectTransform HopeBar;
+	public RectTransform MaterialsBar;
+
+	private const int maxStatValue = 32;
+	private const int startingHeat = 16;
+	private const int startingFood = 16;
+	private const int startingHope = 16;
+	private const int startingMaterials = 16;
 	
 	public static int Heat { get; private set; }
 	public static int Food { get; private set; }
@@ -12,11 +25,30 @@
 		Materials += mod.Materials;
 	}
 	
-	public static void Reset() {
-		Heat = 16;
-		Food = 16;
-		Hope = 16;
-		Materials = 16;
+	public static void ResetStats() {
+		Heat = startingHeat;
+		Food = startingFood;
+		Hope = startingHope;
+		Materials = startingMaterials;
+	}
+
+	void Awake() {
+		ResetStats();
+	}
+
+	void Start() {
+		UpdateStatBars();
+	}
+
+	void UpdateStatBars() {
+		HeatBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+				(float) Heat / maxStatValue * 100.0f);
+		FoodBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+				(float) Food / maxStatValue * 100.0f);
+		HopeBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+				(float) Hope / maxStatValue * 100.0f);
+		MaterialsBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+				(float) Materials / maxStatValue * 100.0f);
 	}
 	
 }
