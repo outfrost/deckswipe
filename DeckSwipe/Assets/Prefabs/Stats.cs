f16ed3e27aea56a040054a5f84641026f0ef6cad
outfrost <kotlet.bahn@gmail.com>
2018-08-15 23:29:23 +0200

    Make stat bars respond to changes

--- DeckSwipe/Assets/Prefabs/Stats.cs
+++ DeckSwipe/Assets/Prefabs/Stats.cs
@@ -1,12 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Stats : MonoBehaviour {
-
+	
 	public RectTransform HeatBar;
 	public RectTransform FoodBar;
 	public RectTransform HopeBar;
 	public RectTransform MaterialsBar;
-
+	
 	private const int maxStatValue = 32;
 	private const int startingHeat = 16;
 	private const int startingFood = 16;
@@ -18,11 +19,16 @@ public class Stats : MonoBehaviour {
 	public static int Hope { get; private set; }
 	public static int Materials { get; private set; }
 	
+	private static LinkedList<Stats> changeListeners = new LinkedList<Stats>();
+	
 	public static void ApplyModification(StatsModification mod) {
 		Heat += mod.Heat;
 		Food += mod.Food;
 		Hope += mod.Hope;
 		Materials += mod.Materials;
+		foreach (Stats listener in changeListeners) {
+			listener.UpdateStatBars();
+		}
 	}
 	
 	public static void ResetStats() {
@@ -32,14 +38,18 @@ public class Stats : MonoBehaviour {
 		Materials = startingMaterials;
 	}
 
+	public Stats() {
+		changeListeners.AddLast(this);
+	}
+	
 	void Awake() {
 		ResetStats();
 	}
-
+	
 	void Start() {
 		UpdateStatBars();
 	}
-
+	
 	void UpdateStatBars() {
 		HeatBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
 				(float) Heat / maxStatValue * 100.0f);
