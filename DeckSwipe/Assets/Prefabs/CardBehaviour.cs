03503b21f8dceaf5b29f80d213f5a16ded8bccb9
outfrost <kotlet.bahn@gmail.com>
2018-08-16 03:42:15 +0200

    Add smooth animation for card converging on point
    Swipes temporarily don't remove the card

--- DeckSwipe/Assets/Prefabs/CardBehaviour.cs
+++ DeckSwipe/Assets/Prefabs/CardBehaviour.cs
@@ -1,20 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CardBehaviour : MonoBehaviour {
 
+	private const float animationDuration = 1.0f;
+	
 	public float SwipeThreshold = 1.0f;
-
+	
 	public CardModel Card { private get; set; }
 	
 	private Vector3 snapPosition;
 	private Vector3 dragStartPosition;
 	private Vector3 dragStartPointerPosition;
+	private Vector3 lastDragPosition;
+	private float dragStopTime;
+	private AnimationState animationState;
 	
 	private void Start() {
 		snapPosition = transform.position;
 	}
 	
+	private void Update() {
+		if (animationState != AnimationState.Idle) {
+			float animationProgress = (Time.time - dragStopTime) / animationDuration;
+			float scaledProgress = 0.0f;
+			if (animationState == AnimationState.Converging) {
+				scaledProgress = 0.15f * Mathf.Pow(animationProgress, 3.0f)
+				                 - 1.5f * Mathf.Pow(animationProgress, 2.0f)
+				                 + 2.38f * animationProgress;
+			}
+			if (scaledProgress > 1.0f || animationProgress > 1.0f) {
+				animationState = AnimationState.Idle;
+				transform.position = snapPosition;
+			}
+			else {
+				Vector3 totalDistance = snapPosition - lastDragPosition;
+				transform.position = lastDragPosition + totalDistance * scaledProgress;
+			}
+		}
+	}
+	
 	private void OnMouseDown() {
+		animationState = AnimationState.Idle;
 		dragStartPosition = transform.position;
 		dragStartPointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 	}
@@ -26,17 +53,29 @@ public class CardBehaviour : MonoBehaviour {
 	}
 	
 	private void OnMouseUp() {
+		lastDragPosition = transform.position;
+		dragStopTime = Time.time;
 		if (transform.position.x < snapPosition.x - SwipeThreshold) {
 			Card.PerformLeftDecision();
-			Destroy(gameObject);
+			//Destroy(gameObject);
+			animationState = AnimationState.Converging;
 		}
 		else if (transform.position.x > snapPosition.x + SwipeThreshold) {
 			Card.PerformRightDecision();
-			Destroy(gameObject);
+			//Destroy(gameObject);
+			animationState = AnimationState.Converging;
 		}
 		else {
-			transform.position = snapPosition;
+			animationState = AnimationState.Converging;
 		}
+	}
+
+	private enum AnimationState {
+
+		Idle,
+		Converging,
+		FlyingAway
+
 	}
 	
 }
