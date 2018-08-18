a0350f47a956cc38172cbf33f3abe397dc134011
outfrost <kotlet.bahn@gmail.com>
2018-08-18 20:42:43 +0200

    Actually, it's better to not have a separate method for this

--- DeckSwipe/Assets/Prefabs/CardBehaviour.cs
+++ DeckSwipe/Assets/Prefabs/CardBehaviour.cs
@@ -29,8 +29,22 @@ public class CardBehaviour : MonoBehaviour {
 	}
 	
 	private void Update() {
-		if (animationState != AnimationState.Idle) {
-			AnimateSnap();
+		if (animationState != AnimationState.Idle) { // Animate card snap
+			float animationProgress = (Time.time - dragStopTime) / animationDuration;
+			float scaledProgress = 0.0f;
+			if (animationState == AnimationState.Converging) {
+				scaledProgress = 0.15f * Mathf.Pow(animationProgress, 3.0f)
+				                 - 1.5f * Mathf.Pow(animationProgress, 2.0f)
+				                 + 2.38f * animationProgress;
+			}
+			if (scaledProgress > 1.0f || animationProgress > 1.0f) {
+				animationState = AnimationState.Idle;
+				transform.position = snapPosition;
+			}
+			else {
+				Vector3 totalDisplacement = snapPosition - lastDragPosition;
+				transform.position = lastDragPosition + totalDisplacement * scaledProgress;
+			}
 		}
 	}
 	
@@ -61,24 +75,6 @@ public class CardBehaviour : MonoBehaviour {
 		}
 		else {
 			animationState = AnimationState.Converging;
-		}
-	}
-
-	private void AnimateSnap() {
-		float animationProgress = (Time.time - dragStopTime) / animationDuration;
-		float scaledProgress = 0.0f;
-		if (animationState == AnimationState.Converging) {
-			scaledProgress = 0.15f * Mathf.Pow(animationProgress, 3.0f)
-			                 - 1.5f * Mathf.Pow(animationProgress, 2.0f)
-			                 + 2.38f * animationProgress;
-		}
-		if (scaledProgress > 1.0f || animationProgress > 1.0f) {
-			animationState = AnimationState.Idle;
-			transform.position = snapPosition;
-		}
-		else {
-			Vector3 totalDisplacement = snapPosition - lastDragPosition;
-			transform.position = lastDragPosition + totalDisplacement * scaledProgress;
 		}
 	}
 	
