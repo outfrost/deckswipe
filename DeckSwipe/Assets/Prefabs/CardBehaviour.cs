63bf88310f88ac14a50ccf804f3d17f36b958bd4
outfrost <kotlet.bahn@gmail.com>
2018-08-18 20:30:50 +0200

    Refactor animation code slightly for clarity

--- DeckSwipe/Assets/Prefabs/CardBehaviour.cs
+++ DeckSwipe/Assets/Prefabs/CardBehaviour.cs
@@ -30,21 +30,7 @@ public class CardBehaviour : MonoBehaviour {
 	
 	private void Update() {
 		if (animationState != AnimationState.Idle) {
-			float animationProgress = (Time.time - dragStopTime) / animationDuration;
-			float scaledProgress = 0.0f;
-			if (animationState == AnimationState.Converging) {
-				scaledProgress = 0.15f * Mathf.Pow(animationProgress, 3.0f)
-				                 - 1.5f * Mathf.Pow(animationProgress, 2.0f)
-				                 + 2.38f * animationProgress;
-			}
-			if (scaledProgress > 1.0f || animationProgress > 1.0f) {
-				animationState = AnimationState.Idle;
-				transform.position = snapPosition;
-			}
-			else {
-				Vector3 totalDistance = snapPosition - lastDragPosition;
-				transform.position = lastDragPosition + totalDistance * scaledProgress;
-			}
+			AnimateSnap();
 		}
 	}
 	
@@ -55,9 +41,9 @@ public class CardBehaviour : MonoBehaviour {
 	}
 	
 	private void OnMouseDrag() {
-		Vector3 positionDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - dragStartPointerPosition;
-		positionDelta.z = dragStartPosition.z;
-		transform.position = dragStartPosition + positionDelta;
+		Vector3 displacement = Camera.main.ScreenToWorldPoint(Input.mousePosition) - dragStartPointerPosition;
+		displacement.z = dragStartPosition.z;
+		transform.position = dragStartPosition + displacement;
 	}
 	
 	private void OnMouseUp() {
@@ -75,6 +61,24 @@ public class CardBehaviour : MonoBehaviour {
 		}
 		else {
 			animationState = AnimationState.Converging;
+		}
+	}
+
+	private void AnimateSnap() {
+		float animationProgress = (Time.time - dragStopTime) / animationDuration;
+		float scaledProgress = 0.0f;
+		if (animationState == AnimationState.Converging) {
+			scaledProgress = 0.15f * Mathf.Pow(animationProgress, 3.0f)
+			                 - 1.5f * Mathf.Pow(animationProgress, 2.0f)
+			                 + 2.38f * animationProgress;
+		}
+		if (scaledProgress > 1.0f || animationProgress > 1.0f) {
+			animationState = AnimationState.Idle;
+			transform.position = snapPosition;
+		}
+		else {
+			Vector3 totalDisplacement = snapPosition - lastDragPosition;
+			transform.position = lastDragPosition + totalDisplacement * scaledProgress;
 		}
 	}
 	
