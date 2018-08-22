d13c060bfc0c0cf17e01c16cf0d39db49c70aab3
outfrost <kotlet.bahn@gmail.com>
2018-08-23 01:58:32 +0200

    Fix indentation

--- DeckSwipe/Assets/Prefabs/Game.cs
+++ DeckSwipe/Assets/Prefabs/Game.cs
@@ -17,31 +17,31 @@ public class Game : MonoBehaviour {
 		}
 		else if (Stats.Food == 0) {
 			SpawnCard(new CardModel(
-				new GameOverCardOutcome(),
-				new GameOverCardOutcome()));
+					new GameOverCardOutcome(),
+					new GameOverCardOutcome()));
 		}
 		else if (Stats.Hope == 0) {
 			SpawnCard(new CardModel(
-				new GameOverCardOutcome(),
-				new GameOverCardOutcome()));
+					new GameOverCardOutcome(),
+					new GameOverCardOutcome()));
 		}
 		else if (Stats.Materials == 0) {
 			SpawnCard(new CardModel(
-				new GameOverCardOutcome(),
-				new GameOverCardOutcome()));
+					new GameOverCardOutcome(),
+					new GameOverCardOutcome()));
 		}
 		else {
 			SpawnCard(new CardModel(
-				new CardActionOutcome(-4, 4, -2, 2),
-				new CardActionOutcome(2, 0, 4, -2)));
+					new CardActionOutcome(-4, 4, -2, 2),
+					new CardActionOutcome(2, 0, 4, -2)));
 		}
 	}
-
+	
 	public void RestartGame() {
 		Stats.ResetStats();
 		DrawNextCard();
 	}
-
+	
 	private void SpawnCard(CardModel card) {
 		CardBehaviour cardInstance = Instantiate(CardPrefab, SpawnPosition, Quaternion.Euler(0.0f, -180.0f, 0.0f));
 		cardInstance.Card = card;
