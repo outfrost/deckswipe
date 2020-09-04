90556400c5d63ee3ccb08ab620df62753bac8d38
outfrost <kotlet.bahn@gmail.com>
2020-09-05 00:33:38 +0200

    Shorten types in InputDispatcher

--- DeckSwipe/Assets/DeckSwipe/World/InputDispatcher.cs
+++ DeckSwipe/Assets/DeckSwipe/World/InputDispatcher.cs
@@ -3,62 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace DeckSwipe.World {
-	
+
+	using KeyHandler = Action<KeyCode>;
+	using KeyHandlerMap = Dictionary<KeyCode, LinkedList<Action<KeyCode>>>;
+
 	public class InputDispatcher : MonoBehaviour {
-		
-		public delegate void KeyDownHandler(KeyCode keyCode);
-		
-		public delegate void KeyUpHandler(KeyCode keyCode);
-		
-		private readonly Dictionary<KeyCode, LinkedList<KeyDownHandler>> keyDownHandlers = new Dictionary<KeyCode, LinkedList<KeyDownHandler>>();
-		
-		private readonly Dictionary<KeyCode, LinkedList<KeyUpHandler>> keyUpHandlers = new Dictionary<KeyCode, LinkedList<KeyUpHandler>>();
-		
+
+		private readonly KeyHandlerMap keyDownHandlers = new KeyHandlerMap();
+		private readonly KeyHandlerMap keyUpHandlers = new KeyHandlerMap();
+
 		private void Update() {
 			// Scan registered key inputs and invoke handlers
 			foreach (var entry in keyDownHandlers) {
 				if (Input.GetKeyDown(entry.Key)) {
-					foreach (KeyDownHandler handler in entry.Value) {
+					foreach (KeyHandler handler in entry.Value) {
 						handler(entry.Key);
 					}
 				}
 			}
 			foreach (var entry in keyUpHandlers) {
 				if (Input.GetKeyUp(entry.Key)) {
-					foreach (KeyUpHandler handler in entry.Value) {
+					foreach (KeyHandler handler in entry.Value) {
 						handler(entry.Key);
 					}
 				}
 			}
-			
 		}
-		
-		public void AddKeyDownHandler(KeyCode keyCode, KeyDownHandler handler) {
-			LinkedList<KeyDownHandler> list;
+
+		public void AddKeyDownHandler(KeyCode keyCode, KeyHandler handler) {
+			LinkedList<KeyHandler> list;
 			try {
-				list = new LinkedList<KeyDownHandler>();
+				list = new LinkedList<KeyHandler>();
 				keyDownHandlers.Add(keyCode, list);
 			}
 			catch (ArgumentException) {
 				list = keyDownHandlers[keyCode];
 			}
-			
+
 			list.AddLast(handler);
 		}
-		
-		public void AddKeyUpHandler(KeyCode keyCode, KeyUpHandler handler) {
-			LinkedList<KeyUpHandler> list;
+
+		public void AddKeyUpHandler(KeyCode keyCode, KeyHandler handler) {
+			LinkedList<KeyHandler> list;
 			try {
-				list = new LinkedList<KeyUpHandler>();
+				list = new LinkedList<KeyHandler>();
 				keyUpHandlers.Add(keyCode, list);
 			}
 			catch (ArgumentException) {
 				list = keyUpHandlers[keyCode];
 			}
-			
+
 			list.AddLast(handler);
 		}
 
 	}
-	
+
 }
