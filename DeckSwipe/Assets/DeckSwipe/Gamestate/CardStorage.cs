cf3c5b318229a4b12e3819170c17b2ebfefa8298
outfrost <kotlet.bahn@gmail.com>
2020-09-15 22:22:41 +0200

    Downsize fallback card collection
    Makes it easier to see when we failed to import an actual collection

--- DeckSwipe/Assets/DeckSwipe/Gamestate/CardStorage.cs
+++ DeckSwipe/Assets/DeckSwipe/Gamestate/CardStorage.cs
@@ -69,55 +69,27 @@ namespace DeckSwipe.Gamestate {
 
 		private void PopulateFallback() {
 			Cards = new Dictionary<int, Card>();
-			Character leadExplorer = new Character("Lead explorer", defaultSprite);
-			Cards.Add(0, new Card("This is a test card text that should appear on screen.",
-					"Here goes",
-					"There goes",
-					new Character("Dr. Bartholomew Oobleck", defaultSprite),
-					new ActionOutcome(-4, 4, -2, 2),
-					new ActionOutcome(2, 0, 4, -2),
+			Character placeholderPerson = new Character("Placeholder Person", defaultSprite);
+			Cards.Add(0, new Card("Placeholder card 1",
+					"A",
+					"B",
+					placeholderPerson,
+					new ActionOutcome(-2, 4, -2, 2),
+					new ActionOutcome(2, 0, 2, -2),
 					new List<ICardPrerequisite>()));
-			Cards.Add(1, new Card("And this is yet another card that you may encounter in the game. Are you having fun thus far?",
-					"No",
-					"Yes",
-					new Character("Peter Port", defaultSprite),
+			Cards.Add(1, new Card("Placeholder card 2",
+					"A",
+					"B",
+					placeholderPerson,
 					new ActionOutcome(-1, -1, -1, -1),
 					new ActionOutcome(2, 2, 2, 2),
 					new List<ICardPrerequisite>()));
-			Cards.Add(2, new Card("The hothouse is very cold. The crops are freezing up. What should we do?",
-					"Turn on the heater in the hothouse",
-					"Put the generator in overdrive",
-					new Character("Oliver Rain", defaultSprite),
+			Cards.Add(2, new Card("Placeholder card 3",
+					"A",
+					"B",
+					placeholderPerson,
 					new ActionOutcome(1, 1, 0, -2),
 					new ActionOutcome(2, 2, -2, -4),
-					new List<ICardPrerequisite>()));
-			Cards.Add(3, new Card("The scouts come across what looks to be a wrecked ship. Should they explore it?",
-					"No",
-					"Yes",
-					leadExplorer,
-					new ActionOutcome(0, 0, 0, 0),
-					new ActionOutcome(0, 0, 0, 0),
-					new List<ICardPrerequisite>()));
-			Cards.Add(4, new Card("The scouts find 2 steam cores and a shipment of wood on board the ship. A crew had been trying to survive in the wreck but everybody's dead now.",
-					"",
-					"",
-					leadExplorer,
-					new ActionOutcome(3, 0, 2, 6),
-					new ActionOutcome(3, 0, 2, 6),
-					new List<ICardPrerequisite>()));
-			Cards.Add(5, new Card("The guards bring a woman to you. She yells \"These swine have been pestering me day in day out! How much does it take to tell them to leave me alone?\".",
-					"Reprimend the guards",
-					"Leave it be",
-					new Character("", defaultSprite),
-					new ActionOutcome(0, 1, 1, 0),
-					new ActionOutcome(0, 0, -1, 0),
-					new List<ICardPrerequisite>()));
-			Cards.Add(6, new Card("An automaton got stuck in a hothouse. What do we do with it?",
-					"Disable the automaton",
-					"Disable the hothouse",
-					new Character("Alice Woodrow", defaultSprite),
-					new ActionOutcome(-2, -2, 0, -2),
-					new ActionOutcome(+2, -4, 0, -2),
 					new List<ICardPrerequisite>()));
 		}
 
