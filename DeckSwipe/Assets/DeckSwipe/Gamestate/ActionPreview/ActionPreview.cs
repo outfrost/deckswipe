6d878a6db958dc48fc8ea6abb45351db906444fe
Róbert Rauch <robert.rauch@student.tuke.sk>
2020-10-11 23:27:15 +0200

    UI changes for action preview

--- DeckSwipe/Assets/DeckSwipe/Gamestate/ActionPreview/ActionPreview.cs
+++ DeckSwipe/Assets/DeckSwipe/Gamestate/ActionPreview/ActionPreview.cs
@@ -6,10 +6,17 @@ using UnityEngine.UI;
 
 public class ActionPreview : MonoBehaviour
 {
-    private Color darkGreen = new Color(0,0.7f,0);
+    private Color darkGreen = new Color(0,0.6f,0);
     private Color lightGreen = new Color(0, 1f, 0);
-    private Color darkRed = new Color(0.7f, 0, 0);
+    private Color darkRed = new Color(0.6f, 0, 0);
     private Color lightRed = new Color(1f, 0, 0);
+    private Color lightGrey = new Color(0.9f, 0.9f, 0.9f);
+
+    [SerializeField]
+    private GameObject leftPreviewGroup;
+
+    [SerializeField]
+    private GameObject rightPreviewGroup;
 
     [SerializeField]
     private CanvasGroup leftPreview;
@@ -31,34 +38,36 @@ public class ActionPreview : MonoBehaviour
 
     public void ChangeLeftActionPreview(float currentStat, float outcomeStat)
     {
-        ChangeActionPreview(currentStat, outcomeStat, leftBottomImage, leftTopImage, leftPreview);
+        ChangeActionPreview(currentStat, outcomeStat, leftBottomImage, leftTopImage, leftPreviewGroup);
     }
 
     public void ChangeRightActionPreview(float currentStat, float outcomeStat)
     {
-        ChangeActionPreview(currentStat, outcomeStat, rightBotomImage, rightTopImage, rightPreview);
+        ChangeActionPreview(currentStat, outcomeStat, rightBotomImage, rightTopImage, rightPreviewGroup);
     }
 
-    private void ChangeActionPreview(float currentStat, float outcomeStat, Image bottomImage, Image topImage, CanvasGroup preview)
+    private void ChangeActionPreview(float currentStat, float outcomeStat, Image bottomImage, Image topImage, GameObject preview)
     {
-        preview.gameObject.SetActive(true);
+        preview.SetActive(true);
         if (currentStat > outcomeStat)
         {
             bottomImage.fillAmount = outcomeStat;
             topImage.fillAmount = currentStat;
-            bottomImage.color = lightRed;
+            //bottomImage.color = lightRed;
+            bottomImage.color = lightGrey;
             topImage.color = darkRed;
         }
         else if (currentStat < outcomeStat)
         {
             bottomImage.fillAmount = currentStat;
             topImage.fillAmount = outcomeStat;
-            bottomImage.color = darkGreen;
-            topImage.color = lightGreen;
+            //bottomImage.color = darkGreen;
+            bottomImage.color = lightGrey;
+            topImage.color = darkGreen;
         }
         else
         {
-            preview.gameObject.SetActive(false);
+            preview.SetActive(false);
         }
     }
 
